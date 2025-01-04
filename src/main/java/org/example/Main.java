6177e9619fe702f124f0f3fb5eeb1696aa1f22a0
SnehalMarutiSalokhe <snehal.maruti.salokhe@gmail.com>
2025-01-04 19:01:03 +0530

    feat(stack): Implemented Stack using LinkedList with push functionality

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,13 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
+            Stack<Integer> stack = new Stack<>();
+            stack.push(70);
+            stack.push(30);
+            stack.push(56);
+            System.out.println("Stack after pushes: " + stack);
 
     }
 }
