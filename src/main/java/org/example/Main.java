624c34391954385e7134470bbbdfa463a2ca9798
SnehalMarutiSalokhe <snehal.maruti.salokhe@gmail.com>
2025-01-04 19:09:42 +0530

    feat(queue): Implemented Queue using LinkedList with enqueue functionality

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,19 +8,26 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
 
-            Stack<Integer> stack = new Stack<>();
-            stack.push(70);
-            stack.push(30);
-            stack.push(56);
-            System.out.println("Stack after pushes: " + stack);
+//            Stack<Integer> stack = new Stack<>();
+//            stack.push(70);
+//            stack.push(30);
+//            stack.push(56);
+//            System.out.println("Stack after pushes: " + stack);
+//
+//        System.out.println("UC2 " + stack);
+//
+//        while (!stack.isEmpty()) {
+//            System.out.println("Top item: " + stack.peek());
+//            System.out.println("Popped item: " + stack.pop());
+//            System.out.println("Stack now: " + stack);
+//        }
 
-        System.out.println("UC2 " + stack);
 
-        while (!stack.isEmpty()) {
-            System.out.println("Top item: " + stack.peek());
-            System.out.println("Popped item: " + stack.pop());
-            System.out.println("Stack now: " + stack);
-        }
+        Queue<Integer> queue = new Queue<>();
+        queue.enqueue(56);
+        queue.enqueue(30);
+        queue.enqueue(70);
+        System.out.println("Queue after enqueues: " + queue);
 
     }
 }
---
+++ src/main/java/org/example/Queue.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.LinkedList;
+
+public class Queue<T> {
+    private LinkedList<T> queue;
+
+    public Queue() {
+        queue = new LinkedList<>();
+    }
+
+    public void enqueue(T item) {
+        queue.addLast(item);
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public int size() {
+        return queue.size();
+    }
+
+    @Override
+    public String toString() {
+        return queue.toString();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
